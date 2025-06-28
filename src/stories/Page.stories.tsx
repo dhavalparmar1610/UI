d169c581079f41cab44f730b9028c73b41e8c720
dhavalparmar1610 <parmardhaval375@gmail.com>
2025-06-28 16:18:16 +0530

    add /storybook

---
+++ src/stories/Page.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+import { Page } from './Page';
+
+const meta: Meta<typeof Page> = {
+  title: 'Components/Page', 
+  component: Page,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Page>;
+
+export const Default: Story = {
+  args: {
+  },
+};
